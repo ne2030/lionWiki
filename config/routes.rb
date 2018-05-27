73ccf1e67546e9679db84ea0d8538e994354bc6b
sebana <38051464+sebana@users.noreply.github.com>
2018-05-27 14:20:53 +0900

    컨트롤러 post main 생성

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'main/index'
+  get 'post/new'
+  get 'post/create'
+  get 'post/edit'
+  get 'post/update'
+  get 'post/search'
+  get 'post/detail'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
